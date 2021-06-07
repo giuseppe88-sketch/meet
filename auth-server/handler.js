ce94ce1d8633439c7d8ce6f6be8621a08325893f
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-07 12:43:09 +0200

    added get token function and test

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -22,7 +22,7 @@ const credentials = {
   redirect_uris: ["https://giuseppe88-sketch.github.io/meet"],
   javascript_origins: ["https://giuseppe88-sketch.github.io", "http://localhost:3000"],
 }; 
-console.log(process.env.CLIENT_ID)
+
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new OAuth2(
   client_id,
@@ -61,3 +61,46 @@ module.exports.getAuthURL = async () => {
     }),
   };
 };
+
+module.exports.getAccessToken = async (event) => {
+  // The values used to instantiate the OAuthClient are at the top of the file
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  // Decode authorization code extracted from the URL query
+  const code = decodeURIComponent(`${event.pathParameters.code}`);
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+     */
+
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(token);
+    });
+  })
+    .then((token) => {
+      // Respond with OAuth token 
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err) => {
+      // Handle error
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err),
+      };
+    });
+}
