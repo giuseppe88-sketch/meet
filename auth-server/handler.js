03a2ba67259b8d6589c7aefeb6b89486836f132a
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-08 11:10:29 +0200

    added headers to catch

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -87,6 +87,9 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(err),
       };
     });
