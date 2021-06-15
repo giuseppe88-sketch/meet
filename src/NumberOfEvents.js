ff57644968f6c21c0050b846e971e711c684b5af
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-15 16:39:48 +0200

    change default number

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component{
 state = {
-    numberOfEvents :32
+    numberOfEvents :20
 }
 handleInputChanged = (event) => {
     const value = event.target.value;
     if (value < 1) {
       return this.setState({
-        errorText: 'Please choose a number between 1 and 32',
+        errorText: 'Please choose a number between 1 and 20',
         numberOfEvents: ''
       });
-    } else if (value > 32) {
+    } else if (value > 20) {
       return this.setState({
-        errorText: 'Please choose a number between 1 and 32',
+        errorText: 'Please choose a number between 1 and 20',
         numberOfEvents: ''
       });
     } else {
