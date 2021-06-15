9b5f25be957e4c88cf12729ec2c2089df0f54e63
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-15 20:24:59 +0200

    added style background

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
     
     return (
       <div className="App">
+           <h1>MeetApp</h1>
            <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/> 
            <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents}/>
            <EventList events={this.state.events} />
