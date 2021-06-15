e67d29ea56aadbb218cc23a4e6a4b6cac1bc5608
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-15 13:14:39 +0200

    refactor update event function

--- src/App.js
+++ src/App.js
@@ -10,14 +10,15 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
+    numberOfEvents: 20
     
   }
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
       if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+        const finalEvent = events.slice(0, this.state.numberOfEvents)
+        this.setState({ events: finalEvent, locations: extractLocations(events) });
       }
     });
   }
@@ -25,13 +26,16 @@ class App extends Component {
   componentWillUnmount(){
     this.mounted = false;
   }
-  updateEvents = (location) => {
+  updateEvents = (location, eventCount) => {
+    
     getEvents().then((events) => {
       const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
+      events.slice(0, eventCount) :
+      events.filter((event) => event.location === location).slice(0, eventCount)
+      
       this.setState({
-        events: locationEvents
+        events: locationEvents,
+        numberOfEvents: eventCount
       });
     });
   }
@@ -40,7 +44,7 @@ class App extends Component {
     return (
       <div className="App">
            <CitySearch locations={this.state.locations} updateEvents={this.updateEvents}/> 
-           <NumberOfEvents numberOfEvents={this.state.numberOfEvents}/>
+           <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents}/>
            <EventList events={this.state.events} />
       </div>
     );
