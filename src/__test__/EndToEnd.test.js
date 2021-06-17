af4ed0bd4c88862934095b70feddd0941391f8c5
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-17 22:41:19 +0200

    added end-to-end test filter city

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -31,3 +31,34 @@ describe('show/hide an event details', () => {
         expect(eventDetails).toBeNull();
       });
   });
+
+  describe('Filter events by city', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+      jest.setTimeout(30000);
+      browser = await puppeteer.launch();
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.city');
+    });
+
+
+    afterAll(() => {
+        browser.close();
+      });
+      test('When user hasn’t searched for a city, show upcoming events from all cities', async () => {
+         const allEvent = await page.$('.event');
+         expect(allEvent).toBeDefined();
+      })
+      test('user should see a list of suggestion when they search for a city', async () => {
+      const searchCity = await page.$('.city');
+      await searchCity.type('input[name=value]', 'berlin');
+      const suggestions = await page.$$('.suggestions li')
+      expect(suggestions).toBeDefined();
+      })
+      test('user can select a city from the suggest list', async () => {
+         const suggestionsCityList = await page.$('.suggestions')
+         expect(suggestionsCityList).toBeDefined();
+      })
+    });
