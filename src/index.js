e40255dca49bc76cb6b675ebc1385283873d6cd8
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-18 11:11:19 +0200

    added atatus

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+atatus.config('c2c78604682d4220a2c18ae28ee5d429').install();
+
