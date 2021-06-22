f72304d9ce6ce85c5ae606db076d636f7b96bb05
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-06-22 15:34:25 +0200

    change if statement getEvents

--- src/api.js
+++ src/api.js
@@ -34,7 +34,7 @@ export const getEvents = async () => {
   if (!navigator.onLine) {
     const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data?JSON.parse(events).events:[];;
+    return JSON.parse(data).events;
   }
   const token = await getAccessToken();
 
